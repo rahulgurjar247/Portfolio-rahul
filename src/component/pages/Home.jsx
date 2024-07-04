15052b84d408195a35cd8f869dd6d27a525bc45d
rahul <rahulgurjar824582@gmail.com>
2024-07-04 23:28:58 +0530

    change color plate and make home page

--- src/component/pages/Home.jsx
+++ src/component/pages/Home.jsx
@@ -1,7 +1,29 @@
+import icon from "../../assets/developer.png"
+  
+const add = {
+  address: icon
+}
 function Home() {
   return (
-    <div className="rightside">
-      <h1>Home</h1>
+    <div className="rightside ">
+      <div className="Home">
+        <div className="left">
+          <h2>Hi there,</h2>
+          <h1>
+            My name is <span>Rahul gurjar</span> <br />I am a Full-Stack
+            Developer
+          </h1>
+          <div>
+            <button id="getInTouch"> Get In Touch</button>
+            <button id="ResumeDownload">
+              <a href="">Download resume</a>
+            </button>
+          </div>
+        </div>
+        <div className="right">
+          <img src={add.address} className="devloper" alt="devloper rahul" />
+        </div>
+      </div>
     </div>
   );
 }
