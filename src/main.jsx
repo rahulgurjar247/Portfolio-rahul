7d45e0c72dd21638d219aee653ea04278b8ef4f4
rahul <rahulgurjar824582@gmail.com>
2024-08-27 09:39:41 +0530

    cleanup file

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,13 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, BrowserRouter } from "react-router-dom";
 import About from "./component/pages/about.jsx";
 import Home from "./component/pages/Home.jsx";
 import Project from "./component/pages/project.jsx";
 import Skill from "./component/pages/skill.jsx";
 import Achievement from "./component/pages/achievement.jsx";
 import Contact from "./component/pages/contact.jsx";
-import Navbar from "./component/navigationbar/navbar.jsx";
 
 const router = createBrowserRouter([
   {
